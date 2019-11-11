75223e317fe5283e2586b3f7aee3b6b6931dcbf9
bustenchops <mmmm_donut@msn.com>
2019-11-11 16:45:51 -0500

    temphumidyshort is the one to use for the data collection

--- test.py
+++ test.py
@@ -1,8 +1,13 @@
 import glob
+import os
 
 location = '/home/pi/'
 
-fileset = [file for file in glob.glob(location + "*.txt")]
+fileset = [file for file in glob.glob(location + "*.hdf5")]
 
+for file in fileset:
+    print(file)
+
+fileset.sort(reverse=True)
 for file in fileset:
     print(file)

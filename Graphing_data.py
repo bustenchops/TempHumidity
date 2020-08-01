cecd646a4c6e0bcac9c335f5c7fec99b0fd49e53
bustenchops <mmmm_donut@msn.com>
2020-07-31 23:39:15 -0400

    finished rough draft of all three files. Climateupdate - works BME280program - Need to test on pi Graphing_data - works - may need tweaking of the graphing though

--- Graphing_data.py
+++ Graphing_data.py
@@ -17,7 +17,6 @@ class rangeuserinput():
         self.ticks = None
 
     def definedtime(self):
-        print('Input start date yyyy-mm-dd')
         self.start_date = input('Input start date yyyy-mm-dd')
         self.end_date = input('Input end date (inclusive) yyyy-mm-dd')
         self.ticks = input('Input the interval for tick labels in hours')
@@ -35,8 +34,7 @@ class findandplot():
         self.ending_formatted = None
         self.date_span_timedelta = None
         self.date_span = None
-        self.file_name = None
-        self.climate_unixdata = []
+        self.climate_unixdata = None
         self.date_span_short = None
         self.timestamp_start = None
         self.timestamp_end = None
@@ -78,7 +76,7 @@ class findandplot():
             self.timeentryindex_end = self.timeentryindex_temp_end[0][0]
             print('nearest first entry in climatedata:', self.nearestvalue_end, ' at: ', self.timeentryindex_end)
             self.index_tracker = self.timeentryindex_start
-            if self.timeentryindex_end > self.timeentryindex_start:
+            while self.timeentryindex_end > self.timeentryindex_start:
                 self.climate_time_array_tempvalue = datetime.datetime.fromtimestamp(f['compiled_data'][self.index_tracker, 0])
                 self.climate_time_array.append(self.climate_time_array_tempvalue.strftime('%Y-%m-%d_%Hh'))
                 self.climate_temp_array.append(f['compiled_data'][self.index_tracker, 1])
@@ -88,6 +86,7 @@ class findandplot():
                 self.data_humidity_array.append(f['compiled_data'][self.index_tracker, 6])
                 self.data_baro_array.append(f['compiled_data'][self.index_tracker, 7])
                 self.index_tracker += 1
+                self.timeentryindex_start += 1
             f.close()
 
 
@@ -96,11 +95,11 @@ class findandplot():
         location_f = './home/climatedata/plots/' + namefile_
 
         # Sets figure size in inches
-        fig = plt.figure(figsize=(8, 10))
+        fig = plt.figure(figsize=(20, 60))
 
         ax = fig.add_subplot(3, 1, 1)
         ax.scatter(self.climate_time_array, self.climate_temp_array, s=3, color=[0,0,0], label='weather station')
-        ax.scatter(self.climate_time_array, self.data_temp_array, s=3, color=[0, 0, 255], label='room3507')
+        ax.scatter(self.climate_time_array, self.data_temp_array, s=3, color=[0,0,1], label='room3507')
         # Here we set x and y labels as strings, they can be whatever you want them to be
         ax.set_xlabel('Time')
         ax.set_ylabel('Temp oC')
@@ -120,7 +119,7 @@ class findandplot():
 
         bx = fig.add_subplot(3, 1, 2)
         bx.scatter(self.climate_time_array, self.climate_humidity_array, s=3, color=[0, 0, 0], label='weather station')
-        bx.scatter(self.climate_time_array, self.data_humidity_array, s=3, color=[0, 0, 255], label='room3507')
+        bx.scatter(self.climate_time_array, self.data_humidity_array, s=3, color=[0, 0, 1], label='room3507')
         #Here we set x and y labels as strings, they can be whatever you want them to be
         bx.set_xlabel('Time')
         bx.set_ylabel('% Humidity')
@@ -136,18 +135,19 @@ class findandplot():
         for label in tempaxis2:
             label.set_visible(False)
 
-        dx = fig.add_subplot(3, 1, 2)
+        dx = fig.add_subplot(3, 1, 3)
         dx.scatter(self.climate_time_array, self.climate_baro_array, s=3, color=[0, 0, 0], label='weather station')
-        dx.scatter(self.climate_time_array, self.data_baro_array, s=3, color=[0, 0, 255], label='room3507')
+        dx.scatter(self.climate_time_array, self.data_baro_array, s=3, color=[0, 0, 1], label='room3507')
         #Here we set x and y labels as strings, they can be whatever you want them to be
         dx.set_xlabel('Time')
-        dx.set_ylabel('Pressure kPa')
+        dx.set_ylabel('Pressure (kPa)')
         #Here we eliminate the top and righthand parts of the box enclosing the figure
         dx.spines['right'].set_visible(False)
         dx.spines['top'].set_visible(False)
         dx.yaxis.set_ticks_position('left')
         dx.xaxis.set_ticks_position('bottom')
         dx.xaxis.set_tick_params(rotation=45)
+        dx.set_ylim([90, 110])
 
         tempaxis3 = dx.xaxis.get_ticklabels()
         tempaxis3 = list(set(tempaxis3) - set(tempaxis3[::rangeofint]))
@@ -155,8 +155,151 @@ class findandplot():
             label.set_visible(False)
 
         plt.legend(loc='lower left')
+        plt.subplots_adjust(left=0.05, bottom=0.16, right=0.95, top=0.95, wspace=0.2, hspace=1.00)
         plt.show()
-        fig.savefig(location_f, tight_layout = True)
+        fig.savefig(location_f, tight_layout = False)
+
+
+    def dotheplot2(self, namefile_, namefile_b, namefile_c, rangeoftick):
+        rangeofint = int(rangeoftick)
+        location_f = './home/climatedata/plots/' + namefile_
+
+        # Sets figure size in inches
+        fig = plt.figure(figsize=(20, 60))
+
+        ax = fig.add_subplot(1, 1, 1)
+        ax.scatter(self.climate_time_array, self.climate_temp_array, s=3, color=[0,0,0], label='weather station')
+        ax.plot(self.climate_time_array, self.climate_temp_array, linestyle='solid', linewidth=0.5, color='black')
+        ax.scatter(self.climate_time_array, self.data_temp_array, s=3, color=[0,0,1], label='room3507')
+        ax.plot(self.climate_time_array, self.data_temp_array, linestyle='solid', linewidth=0.5, color='blue')
+        # Here we set x and y labels as strings, they can be whatever you want them to be
+        ax.set_xlabel('Time')
+        ax.set_ylabel('Temp oC')
+
+
+        # Here we eliminate the top and righthand parts of the box enclosing the figure
+        ax.spines['right'].set_visible(False)
+        ax.spines['top'].set_visible(False)
+        ax.yaxis.set_ticks_position('left')
+        ax.xaxis.set_ticks_position('bottom')
+        ax.xaxis.set_tick_params(rotation=45)
+
+        tempaxis1 = ax.xaxis.get_ticklabels()
+        tempaxis1 = list(set(tempaxis1) - set(tempaxis1[::rangeofint]))
+        for label in tempaxis1:
+            label.set_visible(False)
+
+        plt.legend(loc='lower left')
+        plt.subplots_adjust(left=0.05, bottom=0.16, right=0.95, top=0.95, wspace=0.2, hspace=1.00)
+        plt.show()
+        fig.savefig(location_f, tight_layout = False)
+
+        location_f = './home/climatedata/plots/' + namefile_b
+
+        # Sets figure size in inches
+        fig = plt.figure(figsize=(20, 60))
+
+        ax = fig.add_subplot(1, 1, 1)
+        ax.scatter(self.climate_time_array, self.climate_humidity_array, s=3, color=[0, 0, 0], label='weather station')
+        ax.plot(self.climate_time_array, self.climate_humidity_array, linestyle='solid', linewidth=0.5, color='black')
+        ax.scatter(self.climate_time_array, self.data_humidity_array, s=3, color=[0, 0, 1], label='room3507')
+        ax.plot(self.climate_time_array, self.data_humidity_array, linestyle='solid', linewidth=0.5, color='blue')
+        # Here we set x and y labels as strings, they can be whatever you want them to be
+        ax.set_xlabel('Time')
+        ax.set_ylabel('%Humidity')
+
+        # Here we eliminate the top and righthand parts of the box enclosing the figure
+        ax.spines['right'].set_visible(False)
+        ax.spines['top'].set_visible(False)
+        ax.yaxis.set_ticks_position('left')
+        ax.xaxis.set_ticks_position('bottom')
+        ax.xaxis.set_tick_params(rotation=45)
+
+        tempaxis1 = ax.xaxis.get_ticklabels()
+        tempaxis1 = list(set(tempaxis1) - set(tempaxis1[::rangeofint]))
+        for label in tempaxis1:
+            label.set_visible(False)
+
+        plt.legend(loc='lower left')
+        plt.subplots_adjust(left=0.05, bottom=0.16, right=0.95, top=0.95, wspace=0.2, hspace=1.00)
+        plt.show()
+        fig.savefig(location_f, tight_layout=False)
+
+        location_f = './home/climatedata/plots/' + namefile_c
+
+        # Sets figure size in inches
+        fig = plt.figure(figsize=(20, 60))
+
+        ax = fig.add_subplot(1, 1, 1)
+        ax.scatter(self.climate_time_array, self.climate_baro_array, s=3, color=[0, 0, 0], label='weather station')
+        ax.plot(self.climate_time_array, self.climate_baro_array, linestyle='solid', linewidth=0.5, color='black')
+        ax.scatter(self.climate_time_array, self.data_baro_array, s=3, color=[0, 0, 1], label='room3507')
+        ax.plot(self.climate_time_array, self.data_baro_array, linestyle='solid', linewidth=0.5, color='black')
+        # Here we set x and y labels as strings, they can be whatever you want them to be
+        ax.set_xlabel('Time')
+        ax.set_ylabel('Pressure (kPa)')
+
+        # Here we eliminate the top and righthand parts of the box enclosing the figure
+        ax.spines['right'].set_visible(False)
+        ax.spines['top'].set_visible(False)
+        ax.yaxis.set_ticks_position('left')
+        ax.xaxis.set_ticks_position('bottom')
+        ax.xaxis.set_tick_params(rotation=45)
+        ax.set_ylim([90, 110])
+
+        tempaxis1 = ax.xaxis.get_ticklabels()
+        tempaxis1 = list(set(tempaxis1) - set(tempaxis1[::rangeofint]))
+        for label in tempaxis1:
+            label.set_visible(False)
+
+        plt.legend(loc='lower left')
+        plt.subplots_adjust(left=0.05, bottom=0.16, right=0.95, top=0.95, wspace=0.2, hspace=1.00)
+        plt.show()
+        fig.savefig(location_f, tight_layout=False)
+
+        # bx = fig.add_subplot(3, 1, 2)
+        # bx.scatter(self.climate_time_array, self.climate_humidity_array, s=3, color=[0, 0, 0], label='weather station')
+        # bx.scatter(self.climate_time_array, self.data_humidity_array, s=3, color=[0, 0, 1], label='room3507')
+        # #Here we set x and y labels as strings, they can be whatever you want them to be
+        # bx.set_xlabel('Time')
+        # bx.set_ylabel('% Humidity')
+        # #Here we eliminate the top and righthand parts of the box enclosing the figure
+        # bx.spines['right'].set_visible(False)
+        # bx.spines['top'].set_visible(False)
+        # bx.yaxis.set_ticks_position('left')
+        # bx.xaxis.set_ticks_position('bottom')
+        # bx.xaxis.set_tick_params(rotation=45)
+        #
+        # tempaxis2 = bx.xaxis.get_ticklabels()
+        # tempaxis2 = list(set(tempaxis2) - set(tempaxis2[::rangeofint]))
+        # for label in tempaxis2:
+        #     label.set_visible(False)
+        #
+        # dx = fig.add_subplot(3, 1, 3)
+        # dx.scatter(self.climate_time_array, self.climate_baro_array, s=3, color=[0, 0, 0], label='weather station')
+        # dx.scatter(self.climate_time_array, self.data_baro_array, s=3, color=[0, 0, 1], label='room3507')
+        # #Here we set x and y labels as strings, they can be whatever you want them to be
+        # dx.set_xlabel('Time')
+        # dx.set_ylabel('Pressure kPa')
+        # #Here we eliminate the top and righthand parts of the box enclosing the figure
+        # dx.spines['right'].set_visible(False)
+        # dx.spines['top'].set_visible(False)
+        # dx.yaxis.set_ticks_position('left')
+        # dx.xaxis.set_ticks_position('bottom')
+        # dx.xaxis.set_tick_params(rotation=45)
+        #
+        # tempaxis3 = dx.xaxis.get_ticklabels()
+        # tempaxis3 = list(set(tempaxis3) - set(tempaxis3[::rangeofint]))
+        # for label in tempaxis3:
+        #     label.set_visible(False)
+        #
+        # plt.legend(loc='lower left')
+        # plt.subplots_adjust(left=0.05, bottom=0.16, right=0.95, top=0.95, wspace=0.2, hspace=1.00)
+        # plt.show()
+        # fig.savefig(location_f, tight_layout = False)
+
+
+
 
 ####### PROGRAM #########
 
@@ -168,6 +311,14 @@ first_day, last_day, tick_interval = gotime.definedtime()
 #pull the data from the dataset
 pull_data_and_plot = findandplot()
 pull_data_and_plot.reporting_dataset(first_day, last_day)
-nameoftheplotfile= 'plot_' + first_day + '_to_' + last_day + ',hdf5'
-pull_data_and_plot.dotheplot(nameoftheplotfile, tick_interval)
+nameoftheplotfile= 'plot_' + first_day + '_to_' + last_day
 
+typeofplot = input('1.plots on one page.  2.plots on three pages')
+
+if typeofplot == "1":
+    pull_data_and_plot.dotheplot(nameoftheplotfile, tick_interval)
+if typeofplot == "2":
+    nameoftheplotfile1 = 'plot_temp' + first_day + '_to_' + last_day
+    nameoftheplotfile2 = 'plot_humid' + first_day + '_to_' + last_day
+    nameoftheplotfile3 = 'plot_baro' + first_day + '_to_' + last_day
+    pull_data_and_plot.dotheplot2(nameoftheplotfile1, nameoftheplotfile2,nameoftheplotfile3, tick_interval)
